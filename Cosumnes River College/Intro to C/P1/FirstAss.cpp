3baa70971f9278656312c9a5bea8c0f2a218b5aa
Ali Mirabzadeh <ali.mz758@gmail.com>
2018-12-27 00:48:57 -0800

    Create FirstAss.cpp

---
+++ Cosumnes River College/Intro to C/P1/FirstAss.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "The size of a character is " << sizeof(char) << " bytes.\n";
+	cout << "The size of an integer is "  << sizeof(int) << " bytes.\n";
+	cout << "The size of a float is "  << sizeof(float)  << " bytes.\n";
+	cout << "The size of a double is " << sizeof(double) << " bytes.\n";
+	return 0;
+}
