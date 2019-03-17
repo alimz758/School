9f37125e2ff28e95b0eb7f281ee548e4dfba9d03
Ali Mirabzadeh <ali.mz758@gmail.com>
2019-03-16 23:33:27 -0700

    Add files via upload

---
+++ UCLA/CS35L- Software Laboratory/Lab 7/cP.c
@@ -0,0 +1,16 @@
+//
+//  cP.c
+//  
+//
+//  Created by ali mirabzadeh on 3/2/19.
+//
+
+#include <math.h>
+#include <stdio.h>
+
+//trivial c code 
+int main(void)
+{
+    printf("%.17g", cos(sqrt(3.0)));
+    return 0;
+}
