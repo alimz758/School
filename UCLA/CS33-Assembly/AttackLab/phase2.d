d001f63799801f31d81d3cf723405e1530288b37
Ali Mirabzadeh <ali.mz758@gmail.com>
2019-05-19 23:53:24 -0700

    some school projects got added

---
+++ UCLA/CS33-Assembly/AttackLab/phase2.d
@@ -0,0 +1,9 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 6b 79 4f 5a 	mov    $0x5a4f796b,%rdi
+   7:	c3                   	retq   
---
+++ UCLA/CS33-Assembly/AttackLab/phase2.s~
@@ -0,0 +1,2 @@
+mov $0x5a4f796b, %rdi	
+ret
